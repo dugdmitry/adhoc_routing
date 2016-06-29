6b85768ef974ab7e304f8c32e0270c73222504b7
Dmitrii Dugaev <d.dugaev@emw.hs-anhalt.de>
2016-06-29 11:13:45 +0200

    Changed conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
-DEV = "wlan0"
+DEV = "eth0"
 VIRT_IFACE_NAME = "tun0"
 UDS_ADDRESS = "/tmp/uds_socket"
-ABSOLUTE_PATH = "/root/SmartLighting/AdhocRouting/"
+ABSOLUTE_PATH = "/home/fila/AdhocRouting/"
 LOG_LEVEL = "INFO"
 SET_TOPOLOGY_FLAG = True
