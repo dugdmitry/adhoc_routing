089c0cf9920a4e2bdb8051a3b93bd110de70a0ce
Dmitrii Dugaev <d.dugaev@emw.hs-anhalt.de>
2016-07-08 11:38:23 +0200

    Changed loglevel to INFO

--- conf.py
+++ conf.py
@@ -2,6 +2,6 @@ DEV = "wlan0"
 VIRT_IFACE_NAME = "tun0"
 UDS_ADDRESS = "/tmp/uds_socket"
 ABSOLUTE_PATH = "/root/SmartLighting/AdhocRouting/"
-LOG_LEVEL = "DEBUG"
+LOG_LEVEL = "INFO"
 SET_TOPOLOGY_FLAG = True
 MONITORING_MODE_FLAG = False
