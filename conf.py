6fc3ab272cde056f196d441073f4740eda5cb371
Dmitrii Dugaev <d.dugaev@emw.hs-anhalt.de>
2016-07-17 19:51:54 +0200

    Changed iface name to "dsr0". SET_TOPOLOGY_FLAG to False.

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
 DEV = "wlan0"
-VIRT_IFACE_NAME = "tun0"
+VIRT_IFACE_NAME = "dsr0"
 UDS_ADDRESS = "/tmp/uds_socket"
 ABSOLUTE_PATH = "/root/SmartLighting/AdhocRouting/"
 LOG_LEVEL = "INFO"
-SET_TOPOLOGY_FLAG = True
+SET_TOPOLOGY_FLAG = False
 MONITORING_MODE_FLAG = False
