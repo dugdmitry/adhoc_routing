876e45aff4dc1c447fedb5e97f28c91f6fe15918
Dmitrii Dugaev <d.dugaev@emw.hs-anhalt.de>
2016-07-25 18:52:51 +0200

    Changed iface name to "dsr0".

--- conf.py
+++ conf.py
@@ -3,5 +3,5 @@ VIRT_IFACE_NAME = "dsr0"
 UDS_ADDRESS = "/tmp/uds_socket"
 ABSOLUTE_PATH = "/home/fila/AdhocRouting/"
 LOG_LEVEL = "INFO"
-SET_TOPOLOGY_FLAG = True
+SET_TOPOLOGY_FLAG = False
 MONITORING_MODE_FLAG = False
