b3f4efc8a59fc6421e4f401455aea436de7c9541
Dmitrii Dugaev <d.dugaev@emw.hs-anhalt.de>
2017-01-31 16:48:01 +0100

    added TCP_NODELAY option

--- RoutingManager.py
+++ RoutingManager.py
@@ -50,6 +50,9 @@ class Manager(threading.Thread):
         # Create a UDS socket.
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         self.sock.bind(self.server_address)
+
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
         # Listen for incoming connections
         self.sock.listen(1)
         self.connection = None
